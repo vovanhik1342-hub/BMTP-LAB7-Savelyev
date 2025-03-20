078c0e318bb48fa40796d8035348e8e3ee9bbb5d
VOLODYMYR SAVELIEV <vovasavelev009@gmail.com>
2025-03-20 11:57:39 +0200

    Add files via upload

---
+++ BMTP-LAB7-Saveliev/prj/Coveliev-task_7_1/Coveliev-task_7_1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <windows.h> 
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    string word;
+    cout << "Введіть слово (закінчіть символом '#'): ";
+    cin >> word;
+
+    if (word.empty() || word[word.length() - 1] != '#') {
+        cout << "Помилка: слово повинно закінчуватися символом '#'." << endl;
+        cout << "Натисніть Enter для виходу...";
+        cin.ignore();
+        cin.get();
+        return 1;
+    }
+
+    word = word.substr(0, word.length() - 1);
+
+    int ra_count = 0;
+    for (int i = 0; i < word.length() - 1; i++) {
+        if ((word[i] == 'р' || word[i] == 'Р') &&
+            (word[i + 1] == 'а' || word[i + 1] == 'А')) {
+            ra_count++;
+        }
+    }
+
+    if (ra_count > 0) {
+        cout << "Кількість складів 'ра': " << ra_count << endl;
+    }
+    else {
+        int uppercase_count = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word[i] >= 'А' && word[i] <= 'Я') {
+                uppercase_count++;
+            }
+        }
+        cout << "Довжина слова: " << word.length() << endl;
+        cout << "Кількість заголовкових літер: " << uppercase_count << endl;
+    }
+    cout << "Натисніть Enter для виходу...";
+    cin.ignore();
+    cin.get();
+    return 0;
+}
---
+++ BMTP-LAB7-Saveliev/prj/Coveliev-task_7_2/Coveliev-task_7_2.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <windows.h> 
+using namespace std;
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    int array[10];
+    
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> array[i];
+    }
+    cout << "Масив: ";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << array[i] << " ";
+    }
+
+    int maxIndex = 0;
+
+    for (int i = 1; i < 10; i++)
+    {
+        if (array[i] > array[maxIndex])
+        {
+            maxIndex = i;
+        }
+    }
+
+    cout << "\nМаксимальний елемент: " << array[maxIndex] << endl;
+
+    if (maxIndex == 0)
+    {
+        cout << "Перший елемент вже максимальний" << endl;
+    }
+    else
+    {
+        int temp = array[0];
+        array[0] = array[maxIndex];
+        array[maxIndex] = temp;
+    }
+
+    cout << "\nМасив після перестановки:" << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << array[i] << " ";
+    }
+    cin.ignore();
+    cin.get();
+    return 0;
+}
